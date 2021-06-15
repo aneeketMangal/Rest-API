f4ed5ed384f816f09f0603ac6368dd2e8e5824ea
Aneeket <aneeketmangal@gmail.com>
2021-06-15 22:17:34 +0530

    Revert "completed login/register"
    This reverts commit efccaf2d32027fe57dd2f18bc25914120b86e451.

--- forms/post_form.py
+++
@@ -1,5 +0,0 @@
-from forms.imports import *
-
-class PostForm(FlaskForm):
-    post = StringField('post', validators = [DataRequired(), Length(min = 2, max=20000)])
-    submit = SubmitField('login')
--- main.py
+++ main.py
@@ -3,26 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
 from forms.login_form import LoginForm
 from forms.register_form import RegisterForm
-from elements.user.user import User
-# from flask.views import MethodView
-
-from flask_classful import FlaskView, route
-from database.user_database import UserDatabase
+from flask.views import MethodView
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9616543127'
 
-app.config["MONGO_URI"] = "mongodb+srv://admin:aneeketissuperman@cluster0.i38f8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-app.config["CURRENT_USER"] = None
-app.config["CURRENT_USER_NAME"] = "Anonymous"
-app.config["CURRENT_PFP"] = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-app.config["LOGO"] = "https://codesign.com.bd/conversations/content/images/2020/03/Sprint-logo-design-Codesign-agency.png"
-app.config["APP_NAME"] = "The Rest Crest"
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/todo_db"
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
-
-
 
 
 posts = [
@@ -33,66 +21,63 @@ posts = [
         'date': '24.09.01',
         'image': 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
     },
+    {
+        'user': 'Aneeket Mangal', 
+        'userImage': "https://avatars.githubusercontent.com/u/54039514?v=4",
+        'content': 'Champions Never die.',
+        'date': '24.09.01'
+        # 'image': 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+    },
+    {
+        'user': 'Aneeket Mangal', 
+        'userImage': "https://avatars.githubusercontent.com/u/54039514?v=4",
+        'content': 'Champions Never die.',
+        'date': '24.09.01'
+        # 'image': 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+    },
     
-    
-]*3
+]
+
+@app.route("/addpost")
+def addpost():
+    print("sdf")
+    # try:
+    db.todos.insert_one({'title': "todo title", 'body': "todo body"})
+    # except:
+    #     print("not happpening")
+    return jsonify(message="success")
+
+
+
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    userData = {}
-    if(app.config['CURRENT_USER']):
-        userData = app.config['CURRENT_USER'].getInfo()
-    return render_template('index.html', posts = posts, userData = userData)
+    print("home")
+    return render_template('index.html', posts = posts)
 
 @app.route("/register", methods = ['GET', 'POST'])
 def register():
     form = RegisterForm()
     if(form.validate_on_submit()):
-        databaseLog = user_db.checkUserAvailability(form.username.data, form.email.data)
-        if(databaseLog['isAvailable']):
-            user_db.addUser(form.username.data, form.password.data)
-            flash(f'Account created successfully {form.username.data}!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash(databaseLog['errorLog'], 'danger')
-
+        flash(f'Account created successfully {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title = 'Register', form = form)
 
-
-@app.route('/create')
-def create():
-    return render_template('create.html')
-
-@app.route('/login',  methods = ['GET', 'POST'])
+@app.route("/login")
 def login():
     form = LoginForm()
-    if(form.validate_on_submit()):
-        databaseLog = user_db.verifyUserDetails(form.username.data,form.password.data)
-        app.config["CURRENT_USER"] = User(databaseLog['userData'])
-        app.config["CURRENT_USER_NAME"] = databaseLog['userData']['username']
-        app.config["CURRENT_PFP"] = databaseLog['userData']['pfp']
-        temp = app.config["CURRENT_PFP"]
-        print(temp)
-        
-        if(databaseLog['isAvailable']):
-            # user_db.addUser(form.username.data, form.password.data)
-            flash(f'Welcome {form.username.data}!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash(databaseLog['errorLog'], 'danger')
-
     return render_template('login.html', title = 'Login', form = form)
 
-@app.route("/database/adduser")
-def addUser():
-    user_db.test()
+@app.route("/action/update-text")
+def update_text():
+    posts[0]['user'] = "sdfjsifd"
+    print("fdsa")
     return jsonify({'success':1})
 
 
 
 
 if __name__ == '__main__':
-    user_db = UserDatabase(db)
     app.run(debug = True)
