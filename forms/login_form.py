d05aba64763cd12b033f3762ba19b7f5f1ec7498
Aneeket <aneeketmangal@gmail.com>
2021-06-15 22:18:45 +0530

    Revert "initial commit"
    This reverts commit 0c531632da1a95c833292a109d9fc462b60326a4.

--- forms/login_form.py
+++
@@ -1,7 +0,0 @@
-from forms.imports import *
-
-class LoginForm(FlaskForm):
-    username = StringField('usermame', validators = [DataRequired(), Length(min = 2, max = 20)])
-    password = PasswordField('password', validators = [DataRequired()])
-    remember = BooleanField('remember')
-    submit = SubmitField('login')
